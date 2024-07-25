53298e01ec48d0452ddadc5cccfa6ddcea2859cb
goliathPC <goliath@golith.com>
2024-07-25 17:14:30 +0200

    config.py updated to load .env variables

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,10 @@
 import os
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+#load .env variables on your machine
+load_dotenv()
 
 class Settings(BaseSettings):
     MONGODB_URL: str = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
