a533a651ed75b8af684df9376a3f47b708bc41bb
goliathPC <goliath@golith.com>
2024-07-25 16:47:45 +0200

    CORS middleware added to backend main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.api.api_v1.endpoints import event, user
+from starlette.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Adjust to the URL of your frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(event.router, prefix="/api/v1/events", tags=["events"])
 app.include_router(user.router, prefix="/api/v1/users", tags=["users"])
